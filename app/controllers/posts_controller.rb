497addf230e5f06aabb665f6eeae00f25f7974c1
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-01-04 11:36:52 +0900

    既読機能のフロント実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,6 @@ class PostsController < ApplicationController
         end
 
         item = Post.find(params[:id])
-        render json { post: item}
+        render json: { post:item}
     end
 end
